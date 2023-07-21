8c6c4eaecead37691af70c924c4c9087a644de35
aashish <aashish.shrestha@aayulogic.com>
2023-07-21 09:45:56 +0545

    refactor code

--- charts/views.py
+++ charts/views.py
@@ -24,41 +24,42 @@ def show_server_detail(request, pk):
     network_detail = NetworkUsage.objects.filter(server=server).latest('created_at')
     context = {
         "server_name": server.name,
+        "server_ip": server.ip_address,
         "current_user": other_detail.user,
         "boot_time": other_detail.boot_time,
         "cpu_usage": {
-            "user_time": cpu_detail.user_time,
-            "system_time": cpu_detail.system_time,
-            "idle_time": cpu_detail.idle_time,
-            "cpu_count": cpu_detail.cpu_count,
-            'cpu_percentage': cpu_detail.cpu_percentage
+            "User Time": cpu_detail.user_time,
+            "System Time": cpu_detail.system_time,
+            "Idle Time": cpu_detail.idle_time,
+            "Core Count": cpu_detail.cpu_count,
+            'CPU usage percent': cpu_detail.cpu_percentage
         },
         "ram_usage": {
-            "total_ram": ram_usage.total,
-            "used": ram_usage.used,
-            "free": ram_usage.free,
-            "percent": ram_usage.percent,
-            "available": ram_usage.available
+            "Total RAM": ram_usage.display_total,
+            "Used": ram_usage.display_used,
+            "Free": ram_usage.display_free,
+            "Percent": ram_usage.percent,
+            "Available": ram_usage.display_available
         },
         "swap_usage": {
-            "total_swap": swap_usage.total,
-            "used": swap_usage.used,
-            "free": swap_usage.free,
-            "percent": swap_usage.percent
+            "Total Swap": swap_usage.display_total,
+            "Used": swap_usage.display_used,
+            "Free": swap_usage.display_free,
+            "Percent": swap_usage.percent
         },
         "disk_usage": {
-            "total_disk_usage": disk_usage.total,
-            "used": disk_usage.used,
-            "free": disk_usage.free,
-            "percent": disk_usage.percent,
-            "location": disk_usage.location
+            "Total Disk": disk_usage.display_total,
+            "Used": disk_usage.display_used,
+            "Free": disk_usage.display_free,
+            "Percent": disk_usage.percent,
+            "Location": disk_usage.location
         },
         "network_usage": {
-            "byte_sent": network_detail.byte_sent,
-            "bytes_received": network_detail.bytes_received,
-            "packet_sent": network_detail.packet_sent,
-            "packet_received": network_detail.packet_received,
-            "ip_address": network_detail.ip_address
+            "Byte Sent": network_detail.byte_sent,
+            "Byte Received": network_detail.bytes_received,
+            "Packet Sent": network_detail.packet_sent,
+            "Packet Received": network_detail.packet_received,
+            "IP Address": network_detail.ip_address
         }
     }
     return render(request, 'charts/server-detail.html', context=context)
