157bdf7b645e23d5203b9e7b982f7911bdf97c1a
aashish <aashish.shrestha@aayulogic.com>
2023-07-24 09:39:14 +0545

    add logo to sidebar

--- monitor_server/settings.py
+++ monitor_server/settings.py
@@ -20,6 +20,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+ROOT_DIR = os.path.dirname(PROJECT_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -139,8 +144,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(APPS_DIR, 'static/')
-STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(APPS_DIR, 'static'),
+)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static/')
+STATIC_URL = '/static/'
+print("---------------------------------------", STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
